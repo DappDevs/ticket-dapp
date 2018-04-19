fed67f1afad468a25636a943b5f366268e3ad13c
Bryant Eisenbach <fubuloubu@gmail.com>
2018-04-19 14:56:38 -0400

    Update to use IPFS details

--- contracts/TicketBooth.sol
+++ contracts/TicketBooth.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.21;
+pragma solidity ^0.4.20;
 
 /* @title TicketBooth contract
  *
@@ -8,20 +8,16 @@ pragma solidity ^0.4.21;
  *
  * @notice Contract Specification:
  *
- * Contract is created by an event organizer
- * (aka "Ticket Master") who is collecting ETH
- * in order to host an event (each deployed
- * contract is an event).
+ * Contract is created by an event organizer (aka "Ticket Master") who is
+ * collecting ETH in order to host an event (each deployed contract is an event).
  *
- * Users (aka "Ticket Holders") wish to purchase
- * tickets to attend the event. They can purchase
- * exactly 1 ticket each, and can request a refund
- * up until the event is closed.
+ * Users (aka "Ticket Holders") wish to purchase tickets to attend the event.
+ * They can purchase exactly 1 ticket each, and can request a refund up until
+ * the event is closed.
  *
- * The Ticket Master can "punch" a ticket, thereby
- * validating entry for a particular Ticket Holder,
- * and giving that ticket holder an amount of a
- * reward token in return for showing up.
+ * The Ticket Master can "punch" a ticket, thereby validating entry for
+ * a particular Ticket Holder, and giving that ticket holder an amount of
+ * a reward token in return for showing up.
  *
  */
 
@@ -30,10 +26,16 @@ contract TicketBooth
     /* STATE VARIABLES */
     
     // Event meta-data
-    string public eventName;
-    uint public eventDate;
+    string public eventDetailsIPFSHash;
     uint public eventCost; // wei
+    
+    // @dev Keep track of total tickets
+    // Note: total = bought + left
+    uint public ticketsBought;
     uint public ticketsLeft;
+    
+    //
+    bool public eventStarted;
     
     // Organizer
     address public ticketMaster;
@@ -41,6 +43,7 @@ contract TicketBooth
     // Struct for managing user data
     struct User {
         bool hasPaid;
+        bool punched;
         uint pricePaid;
     }
     
@@ -54,24 +57,44 @@ contract TicketBooth
     /* 
      * @notice Initialize Contract Parameters
      *
-     * @param _eventName (string) The event's name
+     * @param _eventDetailsIPFSHash (string) The event's details stored in IPFS
      * @param _eventDate (uint) UTC timestamp of the event's start time
      * @param _eventCost (uint "wei") Cost of purchasing a ticket
      * @param _ticketsLeft (uint) Number of tickets available for purchase
      */
     function TicketBooth (
-        string _eventName,
-        uint _eventDate,
+        string _eventDetailsIPFSHash,
         uint _eventCost,
         uint _ticketsLeft
     )
         public
     {
         ticketMaster = msg.sender;
-        eventName = _eventName;
-        eventDate = _eventDate;
+        eventDetailsIPFSHash = _eventDetailsIPFSHash;
         eventCost = _eventCost;
         ticketsLeft = _ticketsLeft;
+    }
+
+    /* 
+     * @notice Enforce only Ticket Master ACL
+     */
+    modifier onlyTicketMaster()
+    {
+        require(msg.sender == ticketMaster);
+        _;
+    }
+
+    /* 
+     * @notice Update the details of the event
+     * @dev Only ticketMaster may do this
+     *
+     * @param _eventDetailsIPFSHash (string) The event's details stored in IPFS
+     */
+    function updateEventDetails(string _eventDetailsIPFSHash)
+        public
+        onlyTicketMaster()
+    {
+        eventDetailsIPFSHash = _eventDetailsIPFSHash;
     }
 
     /* 
@@ -84,8 +107,8 @@ contract TicketBooth
      */
     function updateTicketsLeft(uint _ticketsLeft)
         public
+        onlyTicketMaster()
     {
-        require(msg.sender == ticketMaster);
         ticketsLeft = _ticketsLeft;
     }
 
@@ -99,7 +122,7 @@ contract TicketBooth
         payable
     {
         // User is below per-user limit of ticket(s)
-        require(!ticketHolders[msg.sender].hasPaid);
+        require(!ticketHolders[msg.sender].hasPaid); // Re-entrancy protection
         
         // Enough money was provided to purchase the ticket
         require(msg.value >= eventCost);
@@ -108,10 +131,12 @@ contract TicketBooth
         require(ticketsLeft > 0); // State check; Underflow protection
         
         // Buy a ticket
-        ticketsLeft -= 1; // Cannot underflow, only decrements by one; re-entrancy protection
+        ticketsLeft -= 1; // Cannot underflow, only decrements by one
+        ticketsBought += 1; // Should not overflow, only increments by one
         
         // Log User's purchase
-        ticketHolders[msg.sender] = User(true, eventCost);
+        ticketHolders[msg.sender].hasPaid = true; // Re-entrancy protection
+        ticketHolders[msg.sender].pricePaid = eventCost;
         emit TicketPurchase(msg.sender, 1, eventCost);
         
         // Refund user's change from purchase (external call)
@@ -124,8 +149,8 @@ contract TicketBooth
     function refundTicket()
         public
     {
-        // It is before the start time
-        require(now < eventDate);
+        // Event hasn't started yet
+        require(!eventStarted);
 
         // User has ticket(s) to refund
         require(ticketHolders[msg.sender].hasPaid); // State check; re-entrancy protection
@@ -134,7 +159,30 @@ contract TicketBooth
         uint pricePaid = ticketHolders[msg.sender].pricePaid;
         delete ticketHolders[msg.sender]; // Remove from storage; re-entrancy protection
         ticketsLeft += 1; // Cannot overflow, limited to amount of ticket holders
+        ticketsBought -= 1; // Cannot underflow, limited matches number of possible refunds
         msg.sender.transfer(pricePaid); // external call
         emit TicketRefund(msg.sender, 1, eventCost);
+    }
+
+    function punchTicket(address _ticketHolder)
+        public
+        onlyTicketMaster()
+    {
+        // Set the event started if it isn't
+        eventStarted = true;
+
+        // Punch Ticket Holder's ticket
+        ticketHolders[_ticketHolder].punched = true;
+    }
+
+    function finalizeEvent()
+        public
+        onlyTicketMaster()
+    {
+        // Ensure that an event happened before withdrawels are possible
+        require(eventStarted);
+
+        // Ticket master gets the funds raised from the event
+        selfdestruct(ticketMaster);
     }
 }
